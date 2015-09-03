dd0f6aa67d4abb65c8c052814d9458b27ffb7d62
sestaton <statonse@gmail.com>
2015-09-03 07:19:14 -0700

    fix for finding gt

--- lib/Tephra/Role/GT.pm
+++ lib/Tephra/Role/GT.pm
@@ -1,38 +1,48 @@
 package Tephra::Role::GT;
 
 use 5.010;
-use Moo::Role;
+use Moose::Role;
+use MooseX::Types::Path::Class;
 use File::Spec;
-use Types::Standard qw(Str);
+#use Types::Standard qw(Str);
 use Log::Any        qw($log);
 
 has gt_exec => (
     is        => 'rw',
-    isa       => 'Str',
+    isa       => 'Path::Class::File',
+    required  => 0,
+    coerce    => 1, 
     reader    => 'get_gt_exec',
     writer    => 'set_gt_exec',
     predicate => 'has_gt_exec',
+    builder   => '_build_gt_exec',
 );
 
-sub find_gt {
+sub _build_gt_exec {
     my $self  = shift;
     my $gtexe = $self->get_gt_exec; # set during class initialization
-
+    #my $gtexe = '/home';
+    
     # check if the executable path was set first
     if (defined $gtexe && -e $gtexe && -x $gtexe) {
-        return ($gtexe);
+        return $gtexe;
     }
-    elsif (! -e $gtexe) {
+    elsif (! defined $gtexe) {
 	my @path = split /:|;/, $ENV{PATH};
+	for my $p (@path) {
+	    my $gt = File::Spec->catfile($p, 'gt');
 
-    for my $p (@path) {
-        my $gt = File::Spec->catfile($p, 'gt');
-
-    if (-e $gt && -x $gt) {
-        return ($gt);
+	    if (-e $gt && -x $gt) {
+		$self->set_gt_exec($gt);
+		return $gt;
+	    }
+	}
     }
     else {
-        $log->error("Unable to find 'gt' executable. Make sure genometools is installed. Exiting.");
+        #$log->error(
+	say STDERR "Unable to find 'gt' executable. Make sure genometools is installed. Exiting."; #);
         exit(1);
     }
 }
+
+1;
