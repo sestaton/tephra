26514c5b0248b1c705b5c9b78d46bfd5c536b03f
sestaton <statonse@gmail.com>
2015-09-25 17:05:17 -0700

    add capture method

--- lib/Tephra/Role/Util.pm
+++ lib/Tephra/Role/Util.pm
@@ -3,6 +3,7 @@ package Tephra::Role::Util;
 use 5.010;
 use Moose::Role;
 use IPC::System::Simple qw(system);
+use Capture::Tiny;
 use Try::Tiny;
 use namespace::autoclean;
 
@@ -12,6 +13,18 @@ sub run_cmd {
 
     try {
 	system([0..5], $cmd);
+    }
+    catch {
+	die "\nERROR: $cmd failed. Here is the exception: $_\n";
+    };
+}
+
+sub capture_cmd {
+    my $self = shift;
+    my ($cmd) = @_;
+
+    try {
+	my @out = capture { system([0..5], $cmd) };
     }
     catch {
 	die "\nERROR: $cmd failed. Here is the exception: $_\n";
