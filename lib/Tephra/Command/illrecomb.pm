a0e44ca7ea24e5663f2f31441f18ab055488cb58
sestaton <statonse@gmail.com>
2015-09-26 07:01:10 -0700

    add command for finding illegitimate recomb

---
+++ lib/Tephra/Command/illrecomb.pm
@@ -0,0 +1,9 @@
+package Tephra::Command::illrecomb;
+# ABSTRACT: Characterize the distribution of illigetimate recombination in a genome.
+
+use 5.010;
+use strict;
+use warnings;
+use Tephra -command;
+
+1;
