f9a0396693644be5d2aaa4f5de60f9f420152a31
sestaton <statonse@gmail.com>
2015-10-02 12:31:46 -0700

    add options for writing only stats/seqs involved in ill. rec. events

--- lib/Tephra/Genome/IllRecombination.pm
+++ lib/Tephra/Genome/IllRecombination.pm
@@ -12,25 +12,50 @@ use Bio::Seq;
 use Bio::SeqIO;
 use Bio::AlignIO;
 use Bio::SearchIO;
-use Bio::Tools::Run::StandAloneBlast;
+use Parallel::ForkManager;
 use Statistics::Descriptive;
+use Time::HiRes qw(gettimeofday);
 use Log::Any qw($log);
 use namespace::autoclean;
 
+use Data::Dump;
+
 with 'Tephra::Role::Util';
 
-has genome => (
+has dir => (
       is       => 'ro',
-      isa      => 'Path::Class::File',
+      isa      => 'Path::Class::Dir',
       required => 1,
       coerce   => 1,
 );
 
-has repeatdb => (
-      is       => 'ro',
-      isa      => 'Path::Class::File',
-      required => 0,
-      coerce   => 1,
+has outfile => (
+    is       => 'ro',
+    isa      => 'Path::Class::File',
+    required => 1,
+    coerce   => 1,
+);
+
+has allstatsfile => (
+    is       => 'ro',
+    isa      => 'Path::Class::File',
+    required => 1,
+    coerce   => 1,
+);
+
+has illrecstatsfile => (
+    is       => 'ro',
+    isa      => 'Path::Class::File',
+    required => 1,
+    coerce   => 1,
+);
+
+has threads => (
+    is        => 'ro',
+    isa       => 'Int',
+    predicate => 'has_threads',
+    lazy      => 1,
+    default   => 1,
 );
 
 has clean => (
@@ -38,14 +63,92 @@ has clean => (
     isa      => 'Bool',
     required => 0,
     default  => 1,
-    );
+);
+
+sub align_features {
+    my $self = shift;
+    my $threads = $self->threads;
+    my $dir = $self->dir;
+
+    my $args = $self->collect_align_args($dir);
+    $self->_remove_singletons($args);
+
+    my $t0 = gettimeofday();
+    my $doms = 0;
+    my $logfile = File::Spec->catfile($dir, 'all_muscle_reports.log');
+    open my $log, '>>', $logfile or die "\nERROR: Could not open file: $logfile\n";
+
+    my $pm = Parallel::ForkManager->new($threads);
+    $pm->run_on_finish( sub { my ($pid, $exit_code, $ident, $exit_signal, $core_dump, $data_ref) = @_;
+			      my $t1 = gettimeofday();
+			      my $elapsed = $t1 - $t0;
+			      my $time = sprintf("%.2f",$elapsed/60);
+			      say $log basename($ident),
+			          " just finished with PID $pid and exit code: $exit_code in $time minutes";
+			} );
+
+    for my $name (keys %$args) {
+	$doms++;
+	$pm->start($name) and next;
+	$self->capture_cmd($args->{$name}{args});
+	$pm->finish(0);
+    }
+
+    $pm->wait_all_children;
+    
+    my $t2 = gettimeofday();
+    my $total_elapsed = $t2 - $t0;
+    my $final_time = sprintf("%.2f",$total_elapsed/60);
+
+    say $log "\n========> Finished running MUSCLE on $doms families in $final_time minutes";
+    close $log;
+
+    my @aligns;
+    for my $k (keys %$args) {
+	push @aligns, $args->{$k}{aln};
+    }
+    return \@aligns;
+}
+
+sub collect_align_args {
+    my $self = shift;
+    my ($dir) = @_;
+    my (@full, @aln, %aln_args);
+    find( sub { push @full, $File::Find::name if -f and /(?:family\d+|singleton_families).fasta$/ }, $dir);
+
+    for my $fam (@full) {
+	my ($name, $path, $suffix) = fileparse($fam, qr/\.[^.]*/);
+	#my $tre = File::Spec->catfile($path, $name.".dnd");
+	my $aln = File::Spec->catfile($path, $name."_muscle-out.fas");
+	#my $dnd = File::Spec->catfile($path, $name."_clustal-out.dnd");
+	my $log = File::Spec->catfile($path, $name."_muscle-out.log");
+	
+	#my $clwcmd  = "clustalw2 -infile=$fam -outfile=$aln 2>$log";
+	my $clwcmd  = "muscle -in $fam -out $aln 2>$log";
+	$aln_args{$name} = { seqs => $fam, args => $clwcmd, aln => $aln };
+    }
+
+    return \%aln_args;
+}
 
 sub find_illigetimate_recombination {
     my $self = shift;
 
-    my $aln_file;
-    my $outfile;
-    my $statsfile;
+    my $alignments = $self->align_features;
+
+    for my $aln_file (@$alignments) {
+	$self->find_align_gaps($aln_file);
+    }
+}
+
+sub find_align_gaps {
+    my $self = shift;
+    my ($aln_file) = @_;
+
+    my $outfile   = $self->outfile;
+    my $statsfile = $self->allstatsfile;
+    my $illrecstatsfile = $self->illrecstatsfile;
+
     my $dr_pid;
     my $gap_stats;
     my $help;
@@ -56,22 +159,25 @@ sub find_illigetimate_recombination {
     my @indels;
     my @flanking_seqs;
     
-    $dr_pid = defined($dr_pid) ? $dr_pid : '10';
+    $dr_pid = defined($dr_pid) ? $dr_pid : '10'; ## make class attribute
     my $cwd = getcwd();
+    open my $illrecstat_fh, '>>', $illrecstatsfile or die "\nERROR: Could not open file: $!";
+    
     my $statstmp = $statsfile.".tmp";
     open my $out, '>>', $outfile or die "\nERROR: Could not open file: $outfile\n";
     open my $stats_out_tmp, '>>', $statstmp or die "\nERROR: Could not open file: $statstmp\n";
     say $stats_out_tmp join "\t", "#LTR_retro_name", "Total_num_gap_sites", "Num_diff_gap_sizes", 
         "Percent_gap_sites", "Mean_gap_size", "Min_gap_size", "Max_gap_size";
 
-    my ($seqs_in_aln,$count) = $self->split_aln($aln_file);
-
+    my ($seqs_in_aln, $count) = $self->split_aln($aln_file);
+    
     for my $fas (@$seqs_in_aln) {
 	my $aln_in = Bio::AlignIO->new(-fh => \*$fas, -format => 'fasta');
 
 	my ($fname, $fpath, $fsuffix) = fileparse($fas, qr/\.[^.]*/);
-	my $seq_out = $fname;
-	$seq_out .= "_gap_flanking_sequences.fasta";
+	my $seq_out = File::Spec->catfile($fpath, $fname."_gap_flanking_sequences.fasta");
+	#say STDERR "seq_out (each_out fh): $seq_out";
+	#$seq_out .= "_gap_flanking_sequences.fasta";
 	open my $each_out, '>>', $seq_out or die "\nERROR: Could not open file: $seq_out\n";
 
 	while ( my $aln = $aln_in->next_aln() ) {
@@ -79,7 +185,7 @@ sub find_illigetimate_recombination {
 	    for my $hash (@{$aln->gap_col_matrix}) {
 		for my $key (keys %$hash) {
 		    $pos++;
-		    if ($hash->{$key} eq '1') { # gap columns are coded as '1'
+		    if ($hash->{$key} == 1) { # gap columns are coded as '1'
 			$gap++;
 			push @indels, $pos;
 		    }
@@ -93,46 +199,50 @@ sub find_illigetimate_recombination {
 
 	    for my $line (@$indel_ranges) {
 		$del++;
-		my ($indel_len, $indel_spos, $indel_epos) = split /\t/, $line;
+		my ($indel_len, $indel_spos, $indel_epos) = split /\|\|/, $line;
 
-		next unless $indel_len >= 10; # Only analyze repeats flanking deletions > 10 bp; Ma et al. 2004, Genome Research
+		# Only analyze repeats flanking deletions > 10 bp; Ma et al. 2004,Genome Research
+		next unless $indel_len >= 10; 
 
 		# What we want to do is search 20 bp on either side of the gap.
 		# This is based on the 1-15 direct repeats found in Arabidopsis... (Devos et al. 2002)
-		# Sunflower may be different, so we search 20 bp.
+		# Other plants may be different, so we search 20 bp.
 		my $upstream_spos   = $indel_spos - 20;
 		my $upstream_epos   = $indel_spos - 1;
 		my $downstream_spos = $indel_epos + 1;
 		my $downstream_epos = $indel_epos + 20;
 
 		if ($upstream_spos < 1 || $upstream_epos > $aln_len) {
-		    say "Deletion $del has a flanking repeat out of bounds.";
+		    warn "Deletion $del has a flanking repeat out of bounds.";
 		    next;
 		}
 		if ($downstream_epos > $aln_len) {
-		    say "Deletion $del has the downstream flanking repeat out of bounds.";
+		    warn "Deletion $del has the downstream flanking repeat out of bounds.";
 		    last;
 		}
 
-		for my $seq ($aln->each_seq) { # $seq "isa" a Bio::LocatableSeq object (which is a part of Bio::PrimarySeq)
+		for my $seq ($aln->each_seq) { # $seq "isa" a Bio::LocatableSeq object 
 		    my $upstr_seq   = $seq->subseq($upstream_spos,   $upstream_epos);
 		    my $downstr_seq = $seq->subseq($downstream_spos, $downstream_epos);
 
 		    my $upstr_id   = $seq->id."_upstr-del-".$del."_".$upstream_spos."-".$upstream_epos;
 		    my $downstr_id = $seq->id."_downstr-del-".$del."_".$downstream_spos."-".$downstream_epos;
 
-		    my $upstream_seqobj = Bio::Seq->new(-seq      => $upstr_seq,
-							-id       => $upstr_id,
-							-alphabet => 'dna');
-
-		    my $downstream_seqobj = Bio::Seq->new(-seq      => $downstr_seq,
-							  -id       => $downstr_id,
-							  -alphabet =>'dna');
-
-		    # Here is where we do the comparison
-		    $self->blast_compare($indel_len, $upstream_seqobj, $downstream_seqobj, $cwd, $each_out);
-
-		    #//////// could call a sub or just do the Levenshtein distance($upstr_seq, $downstr_seq) here \\\\\\\\\
+		    ## Devos et al. 2002 (I think) defined a threshold of 2bp of non-matching bases
+		    ## following a gap
+		    my @ugaps = ($upstr_seq =~ /(\-+$)/g);
+		    my @dgaps = ($downstr_seq =~ /(^\-+)/g);
+		    if (@ugaps < 2 && @dgaps < 2) {
+			$self->bl2seq_compare({ indel_length   => $indel_len, 
+						upstream_seq   => $upstr_seq, 
+						downstream_seq => $downstr_seq, 
+						upstream_id    => $upstr_id, 
+						downstream_id  => $downstr_id, 
+						fas_name       => $fname, 
+						fas_path       => $fpath, 
+						seqs_fh        => $each_out,
+					        stats_fh       => $illrecstat_fh });
+		    }
 		}
 	    }
 	}
@@ -145,10 +255,10 @@ sub find_illigetimate_recombination {
 	unlink $seq_out;
 	unlink $gap_stats;
     }
-
     close $out;
     close $stats_out_tmp;
-
+    close $illrecstat_fh;
+    
     $self->collate_gap_stats($statstmp, $statsfile);
     unlink $statstmp;
 }
@@ -163,12 +273,12 @@ sub split_aln {
     my $count = 0;
     my $fcount = 1;
     my @split_files;
-    $iname =~ s/\.fa.*//;     # clean up file name like seqs.fasta.1
+    $iname =~ s/\.fa.*//;
     my $cwd = getcwd();
     
     my $tmpiname = $iname."_".$fcount."_XXXX";
     my $fname = File::Temp->new( TEMPLATE => $tmpiname,
-				 DIR      => $cwd,
+				 DIR      => $ipath,
 				 UNLINK   => 0,
 				 SUFFIX   => ".fasta");
 
@@ -180,7 +290,7 @@ sub split_aln {
 	    $fcount++;
 	    $tmpiname = $iname."_".$fcount."_XXXX";
 	    $fname = File::Temp->new( TEMPLATE => $tmpiname,
-				      DIR      => $cwd,
+				      DIR      => $ipath,
 				      UNLINK   => 0,
 				      SUFFIX   => ".fasta");
 
@@ -216,78 +326,133 @@ sub get_indel_range {
 	else {
 	    my $gap_length = ($gap_range->[-1] - $gap_range->[0]) + 1;
 	    push @indel_lengths, $gap_length;
-	    push @indel_ranges, $gap_length."\t".$gap_range->[-1]."\t".$gap_range->[0]."\n";
+	    push @indel_ranges, join "||", $gap_length, $gap_range->[0], $gap_range->[-1];
 	    $gap_range = [ $indel ];
 	}
     }
     return (\@indel_lengths, \@indel_ranges);
 }
 
-sub blast_compare {
+sub bl2seq_compare {
     my $self = shift;
-    my ($indel_len, $upstream_seqobj, $downstream_seqobj, $cwd, $each_out) = @_;
+    my ($args) = @_;
+    my ($indel_len, $upstr_seq, $downstr_seq, $upstr_id, $downstr_id, $fname, $fpath, $each_out, $illrecstat_fh)
+	= @{$args}{qw(indel_length upstream_seq downstream_seq upstream_id downstream_id fas_name fas_path seqs_fh stats_fh)};
 
-    my $dr_pid = 10;
-    my $bl2_out = File::Temp->new(TEMPLATE => "bl2seq_out_XXXX", DIR => $cwd);
-    my $bl2_out_fname = $bl2_out->filename;
+    my $qname = File::Temp->new( TEMPLATE => $fname."_XXXX",
+				 DIR      => $fpath,
+				 UNLINK   => 0,
+				 SUFFIX   => ".fasta");
 
-    my @params = (-F => 'F', -p => 'blastn', -W => 4, -o => $bl2_out_fname);
-    my $factory = Bio::Tools::Run::StandAloneBlast->new(@params);
+    my $rname = File::Temp->new( TEMPLATE => $fname."_XXXX",
+				 DIR      => $fpath,
+				 UNLINK   => 0,
+				 SUFFIX   => ".fasta");
 
-    $factory->bl2seq($upstream_seqobj, $downstream_seqobj);
+    my $out = File::Temp->new( TEMPLATE => $fname."_XXXX",
+			       DIR      => $fpath,
+			       UNLINK   => 0,
+			       SUFFIX   => ".blo");
 
-    my $bl2seq_report = Bio::SearchIO->new(-file => $bl2_out_fname, -format => 'blast');
+    my $outfile = $out->filename;
+    my $qfile   = $qname->filename;
+    my $rfile   = $rname->filename;
+    
+    say $qname join "\n", ">".$upstr_id, $upstr_seq;
+    say $rname join "\n", ">".$downstr_id, $downstr_seq;
+    my $blcmd = "blastn -query $qname -subject $rname -word_size 4 -outfmt 5 -out $outfile";
+    #say STDERR $blcmd;
+    $self->run_cmd($blcmd);
+    unlink $qname, $rname;
 
+    my $bl2seq_report = Bio::SearchIO->new(-file => $outfile, -format => 'blastxml');
+    
     while ( my $result = $bl2seq_report->next_result ) {
-	my $query      = $result->query_name();
-	my $qlen       = $result->query_length();
+	if ($result->num_hits == 0) {
+	    #warn "No hits found.";
+	    unlink $outfile;
+	    return;
+	}
+	else {
+	    #say "HIT FOUND in $upstr_seq and $downstr_seq";# if $debug
+	    my $query = $result->query_name();
+	    my $qlen  = $result->query_length();
+	    while ( my $hit = $result->next_hit ) {
+		my $hitid = $hit->name();
+		my $hlen  = $hit->length;
+		while ( my $hsp = $hit->next_hsp ) {
+		    my $hsplen    = $hsp->length('total');
+		    my $hstart    = $hsp->start('hit');
+		    my $hstop     = $hsp->end('hit');
+		    my $qstart    = $hsp->start('query');
+		    my $qstop     = $hsp->end('query');
+		    my $qstring   = $hsp->query_string;
+		    my $hstring   = $hsp->hit_string;
+		    my $homstring = $hsp->homology_string;
+		    my $hpid      = $hsp->percent_identity;
 
-	while ( my $hit = $result->next_hit ) {
-	    my $hitid    = $hit->name();
+		    my $qend = $qlen - $qstop;
 
-	    while ( my $hsp = $hit->next_hsp ) {
-		my $hsplen    = $hsp->length('total');
-		my $hstart    = $hsp->start('hit');
-		my $hstop     = $hsp->end('hit');
-		my $qstart    = $hsp->start('query');
-		my $qstop     = $hsp->end('query');
-		my $qstring   = $hsp->query_string;
-		my $hstring   = $hsp->hit_string;
-		my $homstring = $hsp->homology_string;
-		my $hpid      = $hsp->percent_identity;
+		    # As defined in Ma et al. 2004 and Devos et al. 2002, 
+		    # we want a match of at least 2 bp ($hsplen),
+		    # no more than 2 bp from the upstream gap terminus ($qend)
+		    # and no more than 2bp from the start of the downstream gap terminus ($hstart).
+		    #
+		    # Finally, if we have the minimum 2bp match in a 20bp string, 
+		    # we should get >= 10% identity ($hpid). This needs some attention bc the match
+		    # string is shorter.
+		    if ($hsplen > 2 && $hpid >= 10 && $qend <= 2 && $hstart <= 2) {    
+			my $qustr  = $upstr_seq;
+			my $hitstr = $downstr_seq;
 
-		if ( $hsplen > 2 && $hpid >= $dr_pid ) {    # $dr_pid ("direct repeat % identity") is 10 or 2/20
-		    my $qustr = $upstream_seqobj->seq;
-		    my $hitstr = $downstream_seqobj->seq;
-
-		    #if ($hstring !~ /$downstream_seqobj->seq/i) {
-		    #print "Why the lack of match?\n";
-		    #next;
-		    #}
-		    my $match_hit_string = uc($hstring);
-
-		    if ($hitstr !~ m/$match_hit_string/i) {
-			#print "$match_hit_string\t$hitstr\n"; 
+			## just testing the indexing for the hit, which is off for some matches
+			my $que_start = index($qustr, $qstring);
+			my $hit_start = index($hitstr, $hstring);
+			
+			#my $match_hit_string = uc($hstring);
+		    
+			#if ($hitstr !~ /$match_hit_string/i) {
+			#print "$match_hit_string\t$hitstr\n";
 			#BioPerl bug? Why is $qstring being reported for $hstring sometimes?
-			next;  #This just ensures the $hstring came from the actual Hit sequence
+			#    next;  #This just ensures the $hstring came from the actual Hit sequence
+			#}
+			#say STDERR join "\t", "Query_ID","Hit_ID","HSP_len","Hit_start",
+		        #    "Hit_stop","Query_start","Query_stop","HSP_PID";
+			#say STDERR join "\t", $query,$hitid,$hsplen,$hstart,$hstop,$qstart,$qstop,$hpid,"\n";
+		    
+			#say STDERR "Gap length        : $indel_len";
+			#say STDERR "Query len         : $qlen";
+			#say STDERR "Query start idx   : $que_start";
+			#say STDERR "Hit len           : $hlen";
+			#say STDERR "Query string      : $qustr\n";
+			#say STDERR "Query match string: ",uc($qstring);
+			#say STDERR "Homology string   : $homstring";
+			#say STDERR "Hit match string  : ",uc($hstring),"\n";
+			#say STDERR "Hit start idx     : $hit_start";
+			#say STDERR "Hit string        : $hitstr\n";
+
+			say $each_out join "\n", ">$upstr_id", $qstring;
+			say $each_out join "\n", ">$downstr_id", $hstring;
+
+			
+			say $illrecstat_fh join "\t", "Query_ID","Hit_ID","HSP_len","Hit_start",
+			    "Hit_stop","Query_start","Query_stop","HSP_PID";
+			say $illrecstat_fh join "\t", $query,$hitid,$hsplen,$hstart,$hstop,$qstart,$qstop,$hpid,"\n";
+
+			say $illrecstat_fh "Gap length        : $indel_len";
+			say $illrecstat_fh "Query len         : $qlen";
+			say $illrecstat_fh "Hit len           : $hlen";
+			say $illrecstat_fh "Query string      : $qustr\n";
+			say $illrecstat_fh "Query match string: ",uc($qstring);
+			say $illrecstat_fh "Homology string   : $homstring";
+			say $illrecstat_fh "Hit match string  : ",uc($hstring),"\n";
+			say $illrecstat_fh "Hit string        : $hitstr\n";
 		    }
-		    say $each_out join "\t", "Query_ID","Hit_ID","HSP_len","Hit_start","Hit_stop","Query_start","Query_stop","HSP_PID";
-		    say $each_out join "\t", $query,$hitid,$hsplen,$hstart,$hstop,$qstart,$qstop,$hpid,"\n";
-
-
-		    say $each_out "Gap length        : $indel_len";
-		    say $each_out "Query len         : $qlen";
-		    say $each_out "Hit len           : ",$hit->length;
-		    say $each_out "Query string      : $qustr\n";
-		    say $each_out "Query match string: ",uc($qstring);
-		    say $each_out "Homology string   : $homstring";
-		    say $each_out "Hit match string  : ",uc($hstring),"\n";
-		    say $each_out "Hit string        : $hitstr\n";
-
 		}
 	    }
 	}
     }
+    unlink $outfile;
 }
 
 sub get_stats {
@@ -307,7 +472,7 @@ sub get_stats {
     my $min = $stat->min;
     my $max = $stat->max;
 
-    say $statsout "$fasname\t$gap\t$count\t$gap_percent\t$mean\t$min\t$max";
+    say $statsout join "\t", $fasname, $gap, $count, $gap_percent, $mean, $min, $max;
 
     close $statsout;
     return $gap_stats;
@@ -316,11 +481,12 @@ sub get_stats {
 sub collate {
     my $self = shift;
     my ($file_in, $fh_out) = @_;
-    open my $fh_in, '<', $file_in or die "\nERROR: Could not open file: $file_in\n";
-    while (<$fh_in>) {
-	print $fh_out $_;
-    }
-    close $fh_in;
+    my $lines = do { 
+	local $/ = undef; 
+	open my $fh_in, '<', $file_in or die "\nERROR: Could not open file: $file_in\n";
+	<$fh_in>;
+    };
+    print $fh_out $lines;
 }
 
 sub collate_gap_stats {
@@ -402,6 +568,56 @@ sub collate_gap_stats {
     close $gap_stats_fh_out;
 }
 
+sub _cnv_align_fmt {
+    my $self = shift;
+    my ($aln) = @_;
+
+    my ($name, $path, $suffix) = fileparse($aln, qr/\.[^.]*/);
+    my $fas = File::Spec->catfile($path, $name.".fas");
+
+    my $seqin  = Bio::AlignIO->new(-file => $aln, -format => 'clustalw');
+    my $seqout = Bio::AlignIO->new(-file => ">$fas", -format => 'fasta');
+    
+    my $seqct = 0;
+    #while (my $seqobj = $seqin->next_aln) { $seqct++ if defined $seqobj->seq; }
+    my $index = 0; # this is a horrible hack...need to get original ids
+
+    #for my $v (1..$seqct) {
+	while (my $seqobj = $seqin->next_aln) {
+	    for my $seq ($seqobj->each_seq) {
+		my $id = $seq->id;
+		#my $id = $seqobj->id;
+		#say STDERR "Debug: $id";
+		$id .= "_$index";
+		#say STDERR "Debug: $id";
+		$seq->id($id);
+		$index++;
+	    }
+	    $seqout->write_aln($seqobj);
+	}
+    #}
+    
+    return $fas;
+}
+sub _remove_singletons {
+    my $self = shift;
+    my ($args) = @_;
+
+    my @singles;
+    my $seqct = 0;
+    for my $name (keys %$args) {
+	my $db = $args->{$name}{seqs};
+	my $seqio = Bio::SeqIO->new( -file => $db, -format => 'fasta' );
+	while (my $seqobj = $seqio->next_seq) { $seqct++ if defined $seqobj->seq; }
+	if ($seqct < 2) {
+	    push @singles, $name;
+	    unlink $db;
+	}
+	$seqct = 0;
+    }
+
+    delete $args->{$_} for @singles;
+}
 
 __PACKAGE__->meta->make_immutable;
 
