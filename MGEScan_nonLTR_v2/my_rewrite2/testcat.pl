9e218ad06a45ba7bb660e3840b74a0da6994b04b
sestaton <statonse@gmail.com>
2015-11-05 17:57:23 -0800

    remove test files :wq
    Former-commit-id: 7c1a461ae4711694ca43dfcdfe42f90a4a1040cf

--- MGEScan_nonLTR_v2/my_rewrite2/testcat.pl
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-use File::Spec;
-use Data::Printer;
-
-my $seqdir = File::Spec->catdir('data', 'info', 'full');
-my @all_clade = ('CR1', 'I', 'Jockey', 'L1', 'L2', 'R1', 'RandI', 'Rex', 'RTE', 'Tad1', 'R2','CRE');
-my @clades = File::Spec->catdir($seqdir, $_), map @all_clade;
-p @clades;
