fadc12bd595fd6851e78fd548fa1afc71f8a7a4f
sestaton <statonse@gmail.com>
2016-01-05 15:32:01 -0800

    quiet build; isolate failing tests

--- build/ci/run_tests.sh
+++ build/ci/run_tests.sh
@@ -3,10 +3,10 @@
 dir=`pwd`
 git clone https://github.com/bioperl/bioperl-live.git
 cd bioperl-live
-echo "n" | perl Build.PL
-./Build install
+echo "n" | perl Build.PL 2>&1 > /dev/null
+./Build install 2>&1 > /dev/null
 cd $dir
 
-perl Makefile.PL && make test
+perl Makefile.PL && make && prove -bv t/01-findltrs.t
 
 
