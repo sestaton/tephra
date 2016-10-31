0922674b1681d685ee3cd1663cfca39b6515504a
sestaton <statonse@gmail.com>
2016-10-31 11:01:57 -0700

    separate superfamily and family finding methods

--- lib/Tephra/Command/classifyltrs.pm
+++ lib/Tephra/Command/classifyltrs.pm
@@ -12,13 +12,16 @@ use Tephra::LTR::MakeExemplars;
 
 sub opt_spec {
     return (    
-	[ "genome|g=s",     "The genome sequences in FASTA format used to search for LTR-RTs "                 ],
-	[ "repeatdb|d=s",   "The file of repeat sequences in FASTA format to use for classification "          ], 
-	[ "hitlength|l=i",  "The alignment length cutoff for BLAST hits to the repeat database (Default: 80) " ],
-	[ "percentid|p=i",  "The percent identity cutoff for BLAST hits to the repeat database (Default: 80) " ],
-	[ "gff|f=s",        "The GFF3 file of LTR-RTs in <genome> "                                            ],
-	[ "outdir|o=s",     "The output directory for placing categorized elements "                           ],
-	[ "threads|t=i",    "The number of threads to use for clustering coding domains "                      ],
+	[ "genome|g=s",     "The genome sequences in FASTA format used to search for LTR-RTs "                       ],
+	[ "repeatdb|d=s",   "The file of repeat sequences in FASTA format to use for classification "                ], 
+	[ "hitlength|l=i",  "The alignment length cutoff for BLAST hits to the repeat database (Default: 80) "       ],
+	[ "percentid|p=i",  "The percent identity cutoff for BLAST hits to the repeat database (Default: 80) "       ],
+	[ "gff|f=s",        "The GFF3 file of LTR-RTs in <genome> "                                                  ],
+	[ "outdir|o=s",     "The output directory for placing categorized elements "                                 ],
+	[ "threads|t=i",    "The number of threads to use for clustering coding domains "                            ],
+	[ "percentcov|c=i", "The percent coverage cutoff for the shorter element in pairwise matches (Default: 50) " ],
+        [ "percentid|p=i",  "The percent identity cutoff for classification of pairwise matches (Default: 80) "      ],
+        [ "hitlen|l=i",     "The minimum length for classifying pairwise BLAST hits (Default: 80) "                  ],
     );
 }
 
@@ -43,10 +46,11 @@ sub validate_args {
 sub execute {
     my ($self, $opt, $args) = @_;
 
-    my $some = _classify_ltr_predictions($opt);
+    my $gffs = _classify_ltr_superfamilies($opt);
+    my $somb = _classify_ltr_families($opt, $gffs);
 }
 
-sub _classify_ltr_predictions {
+sub _classify_ltr_superfamilies {
     my ($opt) = @_;
 
     my $genome   = $opt->{genome};
@@ -76,54 +80,50 @@ sub _classify_ltr_predictions {
     my $gyp_gff = $classify_obj->write_gypsy($gypsy, $header);
     my $cop_gff = $classify_obj->write_copia($copia, $header);
     my $unc_gff = $classify_obj->write_unclassified($features, $header);
+    
+    return ({ gypsy => $gyp_gff, copia => $cop_gff, unclassified => $unc_gff });
+}
+
+sub _classify_ltr_families {
+    my ($opt, $gffs) = @_;
+
+    my $genome   = $opt->{genome};
+    my $repeatdb = $opt->{repeatdb};
+    my $gff      = $opt->{gff};
+    my $outdir   = $opt->{outdir};
+    my $threads  = $opt->{threads} // 1;
+    my $hpcov    = $opt->{percentcov} // 50;
+    my $hpid     = $opt->{percentid} // 80;
+    my $hlen     = $opt->{hitlen} // 80;
+
+    my ($cop_gff, $gyp_gff, $unc_gff) = @{$gffs}{qw(copia gypsy unclassified)};
+
+    for my $file ($cop_gff, $gyp_gff, $unc_gff) {
+	unless (defined $file && -e $file) {
+	    say "\nERROR: There was an error generating GFF3 for family level classification. Exiting.\n";
+	}
+    }
 
     my $classify_fams_obj = Tephra::Classify::LTRFams->new(
-	genome   => $genome,
-	outdir   => $outdir,
-	threads  => $threads,
+	genome        => $genome,
+	outdir        => $outdir,
+	threads       => $threads,
+	blast_hit_cov => $hpcov,
+        blast_hit_pid => $hpid,
+        blast_hit_len => $hlen,
     );
 
-    my $gyp_dir = $classify_fams_obj->extract_features($gyp_gff);
-    my $gyp_clusters = $classify_fams_obj->cluster_features($gyp_dir);
-    my ($gyp_fams, $gyp_ids, $gyp_ct) = $classify_fams_obj->parse_clusters($gyp_clusters);
-    
-    my $cop_dir = $classify_fams_obj->extract_features($cop_gff);
-    my $cop_clusters = $classify_fams_obj->cluster_features($cop_dir);
-    my ($cop_fams, $cop_ids, $cop_ct) = $classify_fams_obj->parse_clusters($cop_clusters);
-    
-    my $unc_dir = $classify_fams_obj->extract_features($unc_gff);
-    my $unc_clusters = $classify_fams_obj->cluster_features($unc_dir);
-    my ($unc_fams, $unc_ids, $unc_ct) = $classify_fams_obj->parse_clusters($unc_clusters);
-
-    my (%outfiles, %annot_ids);
-    @outfiles{keys %$_}  = values %$_ for ($gyp_fams, $cop_fams, $unc_fams);
-    @annot_ids{keys %$_} = values %$_ for ($gyp_ids, $cop_ids, $unc_ids);
-    $classify_fams_obj->combine_families(\%outfiles);
-    $classify_fams_obj->annotate_gff(\%annot_ids, $gff);
-
-    my $gyp_exm_obj = Tephra::LTR::MakeExemplars->new(
-	genome => $genome,
-	dir    => $gyp_dir,
-	gff    => $gyp_gff
-    );
-
-    $gyp_exm_obj->make_exemplars;
-
-    my $cop_exm_obj = Tephra::LTR::MakeExemplars->new(
-	genome => $genome,
-	dir    => $cop_dir,
-	gff    => $cop_gff
-    );
-
-    $cop_exm_obj->make_exemplars;
+    my ($outfiles, $annot_ids) = $classify_fams_obj->make_ltr_families($gffs);
+    $classify_fams_obj->combine_families($outfiles);
+    $classify_fams_obj->annotate_gff($annot_ids, $gff);
     unlink $gyp_gff, $cop_gff, $unc_gff;
 
-    say STDERR '=' x 50;
-    say STDERR join "\t", 'Gypsy_families', 'Gypsy_singletons', 'Copia_families', 'Copia_singletons', 
-        'Unclassified_families', 'Unclassified_singletons';
-    say STDERR join "\t", @{$gyp_ct}{qw(family_count singleton_count)}, @{$cop_ct}{qw(family_count singleton_count)},
-        @{$unc_ct}{qw(family_count singleton_count)};
-    say STDERR '=' x 50;
+    #say STDERR '=' x 50;
+    #say STDERR join "\t", 'Gypsy_families', 'Gypsy_singletons', 'Copia_families', 'Copia_singletons', 
+        #'Unclassified_families', 'Unclassified_singletons';
+    #say STDERR join "\t", @{$gyp_ct}{qw(family_count singleton_count)}, @{$cop_ct}{qw(family_count singleton_count)},
+        #@{$unc_ct}{qw(family_count singleton_count)};
+    #say STDERR '=' x 50;
 }
 
 sub help {
@@ -141,8 +141,9 @@ Required:
 
 Options:
     -t|threads    :   The number of threads to use for clustering coding domains (Default: 1).    
-    -l|hitlength  :   The alignment length cutoff for BLAST hits to the repeat database (Default: 80).
-    -p|percentid  :   The percent identity cutoff for BLAST hits to the repeat database (Default: 80).
+    -c|percentcov :   The percent coverage cutoff for the shorter element in pairwise matches (Default: 50).
+    -p|percentid  :   The percent identity cutoff for classification of pairwise matches (Default: 80).
+    -l|hitlen     :   The minimum length for classifying pairwise BLAST hits (Default: 80).
 
 END
 }
@@ -200,13 +201,24 @@ S. Evan Staton, C<< <statonse at gmail.com> >>
 
  The number of threads to use for clustering coding domains (Default: 1).
 
+=item -c, --percentcov
+
+ The percent coverage cutoff for the shorter element in pairwise matches (Default: 50). This option is used for family-level
+ classifications. Elements will be added to existing families if this criteria is met, among others. This threshold has the 
+ largest impact on family size, so use carefully. Increase for stringency and smaller families, decrease for reduced stringency 
+ and larger families.
+
 =item -l, --hitlength
 
- The alignment length cutoff for BLAST hits to the repeat database (Default: 80). For samples where you do not have a repeat database from a closely related species, it may help to lower this value to increase the number of classified elements.
+ The alignment length cutoff for pairwise BLAST hits (Default: 80). This option is used for family-level                  
+ classifications. It is recommended to not alter this value, as it has little impact. If necessary, it is best to alter this 
+ while holding the percent coverage and percent identity unchanged.
 
 =item -p, --percentid
 
- The percent identity cutoff for BLAST hits to the repeat database (Default: 80). For samples where you do not have a repeat database from a closely related species, it may help to lower this value to increase the number of classified elements.
+ The percent identity cutoff for classification of pairwise matches (Default: 80). This option is used for family-level
+ classifications. Increase for stringency and smaller families, decrease for reduced stringency               
+ and larger families. It is recommended to leave this value unchanged and only change the percent coverage cutoff.
 
 =item -h, --help
 
