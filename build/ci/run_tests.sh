417c4d64cc24577681639eb266f1dc2e5457b04a
sestaton <statonse@gmail.com>
2016-12-01 14:48:11 -0800

    run failing test

--- build/ci/run_tests.sh
+++ build/ci/run_tests.sh
@@ -19,7 +19,7 @@ make
 #cover -test -blib -ignore "blib/lib/Tephra/Command.pm" -report coveralls
 #prove -bv t/04-findtirs.t
 #prove -bv t/0[45]*t
-make test
-#prove -bv t/01-findltrs.t
-#perl -Mblib blib/bin/tephra findltrs -c t/test_data/tephra_ltr_config.yml -g t/test_data/ref.fas -t t/test_data/trnas.fas -d t/test_data/te.hmm --clean --debug
+#make test
+prove -bv t/01-findltrs.t
+perl -Mblib blib/bin/tephra findltrs -c t/test_data/tephra_ltr_config.yml -g t/test_data/ref.fas -t t/test_data/trnas.fas -d t/test_data/te.hmm --clean --debug
 
