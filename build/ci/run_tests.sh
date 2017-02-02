2313848dc782e2f8b2abbb5802618fabcf4e4c45
sestaton <statonse@gmail.com>
2017-02-02 13:06:55 -0800

    run all tests

--- build/ci/run_tests.sh
+++ build/ci/run_tests.sh
@@ -18,8 +18,8 @@ make
 #cover -test -blib -ignore "blib/lib/Tephra/Command.pm" -report coveralls
 #prove -bv t/04-findtirs.t
 #prove -bv t/0[45]*t
-#make test
-prove -bv t/01-findltrs.t
-ls -l t/test_data
-perl -Mblib blib/bin/tephra findltrs -c t/test_data/tephra_ltr_config.yml -g t/test_data/ref.fas -t t/test_data/trnas.fas -d t/test_data/te.hmm --clean --debug
+make test
+#prove -bv t/01-findltrs.t
+#ls -l t/test_data
+#perl -Mblib blib/bin/tephra findltrs -c t/test_data/tephra_ltr_config.yml -g t/test_data/ref.fas -t t/test_data/trnas.fas -d t/test_data/te.hmm --clean --debug
 
