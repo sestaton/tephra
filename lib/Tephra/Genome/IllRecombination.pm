07bc7e59165e6587b6b5a6ea942a91735fb29712
sestaton <1477856+sestaton@users.noreply.github.com>
2018-03-22 12:18:45 -0700

    place warnings on separate line for visibility

--- lib/Tephra/Genome/IllRecombination.pm
+++ lib/Tephra/Genome/IllRecombination.pm
@@ -133,7 +133,7 @@ sub align_features {
     $pm->run_on_finish( sub { my ($pid, $exit_code, $ident, $exit_signal, $core_dump, $data_ref) = @_;
 			      unlink $data_ref->{data}{seqs}, unlink $data_ref->{data}{log};
 			      if ($data_ref->{status} =~ /failed/i) {
-				  say $log "[WARNING]: ",basename($ident), " failed with exit code: $exit_code";
+				  say $log "\n[WARNING]: ",basename($ident), " failed with exit code: $exit_code\n";
 			      }
 			      else {
 				  my $t1 = gettimeofday();
