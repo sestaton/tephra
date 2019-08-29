7b3905ab86f130b32fbcbce55be9b9d49b52d202
Evan Staton <statonse@gmail.com>
2019-08-28 18:59:05 -0700

    update build script

--- build/singularity-build.sh
+++ build/singularity-build.sh
@@ -3,18 +3,16 @@
 set -euo pipefail
 
 vers=$(egrep "our.*VERSION" bin/tephra | sed "s/^.* '//;s/'.*$//")
-#echo $vers
 
 export SINGULARITYENV_LC_ALL=C
 
 mkdir -p image
 
 # build singularity image
-for ver in $vers "latest";
+for ver in "latest";
 do
     sing_vers=$(echo $ver | sed 's/\.[0-9]$//')
     # this is certainly a bug in Singularity to not support semantic versioning
-    #echo $sing_vers
 
     docker run -v \
 	/var/run/docker.sock:/var/run/docker.sock \
@@ -27,12 +25,11 @@ do
 	sestaton/tephra
 
     # sign image
-    #singularity sign image/tephra:${vers}.simg
+    singularity sign image/tephra:${sing_vers}.simg
 
     # push to remote
-    #singularity push image/tephra:${vers}.simg
-
+    singularity push image/tephra:${sing_vers}.simg library://sestaton/default/tephra
 done
 
 # clean up -- this is important because we do not want to commit any images
-#rm -rf image/
+rm -rf image/
