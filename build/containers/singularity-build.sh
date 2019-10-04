42be29677ad7cbf00917fa27c218f9e47cd27fcd
Evan Staton <statonse@gmail.com>
2019-10-04 19:03:54 -0400

    update singularity build script

--- build/containers/singularity-build.sh
+++ build/containers/singularity-build.sh
@@ -4,37 +4,31 @@ set -euo pipefail
 
 vers=$(egrep "our.*VERSION" bin/tephra | sed "s/^.* '//;s/'.*$//")
 
-export SINGULARITYENV_LC_ALL=C
+export SINGULARITYENV_LC_ALL="C"
 export SINGULARITY_LOCALCACHEDIR=${PWD}/image
 export SINGULARITY_TMPDIR=${PWD}/image
 export TMPDIR=${PWD}/image
 
-#-f ${PWD}/image \
-
-mkdir -p image
+mkdir -p $TMPDIR
 
 # build singularity image
-for ver in $vers "latest";
-do
-    sing_vers=$(echo $ver | sed 's/\.[0-9]$//')
-    # this is certainly a bug in Singularity to not support semantic versioning
+docker run -v \
+    /var/run/docker.sock:/var/run/docker.sock \
+    -v $TMPDIR:/output \
+    --privileged \
+    -t \
+    --rm \
+    singularityware/docker2singularity \
+    --name tephra \
+    sestaton/tephra
 
-    docker run -v \
-	/var/run/docker.sock:/var/run/docker.sock \
-	-v ${PWD}/image:/output \
-	--privileged \
-	-t \
-	--rm \
-	singularityware/docker2singularity \
-	--name tephra:$ver \
-	sestaton/tephra
+# sign image
+singularity sign image/tephra.simg
 
-    # sign image
-    singularity sign image/tephra:${sing_vers}.simg
-
-    # push to remote
-    singularity push image/tephra:${sing_vers}.simg library://sestaton/default/tephra:${sing_vers}
-done
+# push to remote
+echo -e "\n=====> Pushing tephra:${vers} and tephra:latest to Sylabs Cloud\n"
+singularity push image/tephra.simg library://sestaton/default/tephra:${vers}
+singularity push image/tephra.simg library://sestaton/default/tephra:latest
 
 # clean up -- this is important because we do not want to commit any images
-rm -rf image/
+rm -rf $TMPDIR
