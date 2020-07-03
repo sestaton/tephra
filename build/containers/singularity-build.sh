4b2ec150aea177e64d22a31d169e771ac82d040e
sestaton <1477856+sestaton@users.noreply.github.com>
2020-07-03 16:57:56 -0700

    update singularity build

--- build/containers/singularity-build.sh
+++ build/containers/singularity-build.sh
@@ -12,23 +12,26 @@ export TMPDIR=${PWD}/image
 mkdir -p $TMPDIR
 
 # build singularity image
+echo -e "\n=====> [TEPHRA]: Pulling Docking image and building Singularity image now.\n"
 docker run -v \
     /var/run/docker.sock:/var/run/docker.sock \
     -v $TMPDIR:/output \
     --privileged \
     -t \
     --rm \
-    singularityware/docker2singularity \
+    quay.io/singularity/docker2singularity \
     --name tephra \
     sestaton/tephra
 
-# sign image
-singularity sign image/tephra.simg
+# -- sign image --
+# NB: This is likely to fail because keys expire on Sylabs Cloud after only a short time. Therefore we warn....
+echo -e "\n=====> [TEPHRA] Attempting to sign image now. If this fails, log on to: https//cloud.sylabs.io/ to update keys or sign image\n"
+singularity sign image/tephra.sif
 
 # push to remote
-echo -e "\n=====> Pushing tephra:${vers} and tephra:latest to Sylabs Cloud\n"
-singularity push image/tephra.simg library://sestaton/default/tephra:${vers}
-singularity push image/tephra.simg library://sestaton/default/tephra:latest
+echo -e "\n=====> [TEPHRA] Pushing tephra:${vers} and tephra:latest to Sylabs Cloud\n"
+singularity push image/tephra.sif library://sestaton/default/tephra:${vers}
+singularity push image/tephra.sif library://sestaton/default/tephra:latest
 
 # clean up -- this is important because we do not want to commit any images
 rm -rf $TMPDIR
