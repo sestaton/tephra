282068afa832d0d0ca9d6f0375bc9a37ca13190d
sestaton <statonse@gmail.com>
2023-04-23 14:43:37 -0500

    add dialogue to the build process

--- build/containers/docker-build.sh
+++ build/containers/docker-build.sh
@@ -3,16 +3,18 @@
 set -euo pipefail
 
 vers=$(egrep "our.*VERSION" bin/tephra | sed "s/^.* '//;s/'.*$//")
-#echo $vers
+echo "=====> Building Docker image for Tephra v$vers"
 
 #--build-arg LC_ALL=C
 # build
 docker build \
 -t sestaton/tephra:$vers .
 
+echo "=====> Tagging Docker image for Tephra v$vers"
 # tag
 docker tag sestaton/tephra:$vers sestaton/tephra:latest
 
+echo "=====> Pushing Docker image for Tephra v$vers to Docker Hub"
 # push
 docker push sestaton/tephra:$vers
 docker push sestaton/tephra:latest
